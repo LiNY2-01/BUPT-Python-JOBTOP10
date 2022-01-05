eca784ecd2f325499ab8cdbe8b32d6dad48f5ff2
LiNY2 <1623482168@qq.com>
2022-01-05 13:43:15 +0800

    modified multi_crawl

--- JOBTOP10/muti_cmd/multi_crawl.py
+++ JOBTOP10/muti_cmd/multi_crawl.py
@@ -15,14 +15,3 @@ class Command(BaseRunSpiderCommand):
             self.crawler_process.crawl(spname, **opts.spargs)
             print("##############此时启动的爬虫##############：" + spname)
         self.crawler_process.start()
-        #spname = args[0]
-        #crawl_defer = self.crawler_process.crawl(spname, **opts.spargs)
-        #if getattr(crawl_defer, 'result', None) is not None and issubclass(crawl_defer.result.type, Exception):
-        #    self.exitcode = 1
-        #else:
-        #    self.crawler_process.start()
-        #    if (
-        #         self.crawler_process.bootstrap_failed
-        #        or hasattr(self.crawler_process, 'has_exception') and self.crawler_process.has_exception
-        #     ):
-        #        self.exitcode = 1

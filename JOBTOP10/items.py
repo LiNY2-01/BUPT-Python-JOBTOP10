a2bc7515a9de6a619413deda6e8509f7f304a446
LiNY2 <1623482168@qq.com>
2022-01-05 13:42:57 +0800

    define items

--- JOBTOP10/items.py
+++ JOBTOP10/items.py
@@ -9,4 +9,7 @@ import scrapy
 class Jobtop10Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    job_title = scrapy.Field()  # 招聘帖子的主题
+    job_date  = scrapy.Field()  # 招聘帖子的发帖时间
+    job_views = scrapy.Field()  # 招聘帖子的浏览次数
+    job_nums  = scrapy.Field()  # 招聘帖子的职位数量

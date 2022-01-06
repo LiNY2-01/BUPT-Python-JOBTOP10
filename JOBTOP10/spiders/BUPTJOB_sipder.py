464eecabaf7b6b7bff0bd60f6a484247e0067018
LiNY2 <1623482168@qq.com>
2022-01-06 13:20:23 +0800

    success

--- JOBTOP10/spiders/BUPTJOB_sipder.py
+++ JOBTOP10/spiders/BUPTJOB_sipder.py
@@ -7,59 +7,41 @@ from selenium import webdriver
 from scrapy import signals
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
+import json
 class BUPTJOBSpider(scrapy.spiders.Spider):
     name="BUPTJOB"
-    allowed_domains = ["https://job.bupt.edu.cn"]
-    start_urls = [
-        "https://job.bupt.edu.cn/frontpage/bupt/html/recruitmentinfoList.html?type=1"]
-
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super(BUPTJOBSpider, cls).from_crawler(crawler, *args, **kwargs)
-        # 创建 浏览器
-        spider.driver = webdriver.Firefox()
-        crawler.signals.connect(spider.spider_closed,
-                                signal=signals.spider_closed)
-        return spider
-
-    def spider_closed(self, spider):
-        spider.driver.close()  # 关闭浏览器
-        print("==========爬虫结束！==========")
-        spider.logger.info('Spider closed:%s', spider.name)
-
-    def check_lastpage(response):
-        if response(".fPage>li:last-child>a::attr(href)").extract() is None:
-            return False
-        else :
-            return True
+    allowed_domains = ["job.bupt.edu.cn"]
+    # start_urls = ['https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo=']
+    pagenum=1
+    def start_requests(self):
+        yield scrapy.Request('https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo=',
+                             callback=self.parse, meta={'method': "stastic"})
+   
     def parse(self, response):
-        spiderflag = 1
-        driver = self.driver
-        while spiderflag:            
-            
-            nxtbtn=driver.find_element("css selector", ".fPage>li:last-child>a")
-            nxtbtn_herf = nxtbtn.get_attribute("href")
-            if nxtbtn_herf is None:
-                print("BUPT last page got!")
-                spiderflag = 0
-                # d = nxtbtn.execute_script('return arguments[0].click();', e)
-    
-            jobpost_div_list = driver.find_elements("css selector",
-                'div[class="infoItem collected recmmonded"]>div[class="left"]')
-            for jobpost in jobpost_div_list:
-                jobpost_link = jobpost.find_element(
-                    "css selector", 'a[class="tit"]').get_attribute("href")
-                print(jobpost_link)
-                jobpost_date = jobpost.find_element("css selector",'span[class="time"]').text
-                te = datetime.strptime(jobpost_date, '%Y-%m-%d')
-                if datetime.strptime(jobpost_date, '%Y-%m-%d') < datetime.strptime('2021-12-29', '%Y-%m-%d'):
-                    spiderflag = 0
-                    break  # 测试时候设定时间为'2021-12-01'，正式执行时候，此处时间按要求设置为'2021-09-01'
-                # driver.get(job.css('a[class="tit"]::attr(href)').extract()[0])
-            driver.execute_script('return arguments[0].click();', nxtbtn)
-            time.sleep(3)
+        data = json.loads(response.body)   # 获取网页的json数据
+        jobpost_list = data['object']['list']
+        print("now pagenum:"+str(data['object']['pageNo']))
+        print("job len:"+str(len(jobpost_list)))
+        for jobpost in jobpost_list:
 
+            if datetime.strptime(jobpost["startTime"], '%Y-%m-%d %H:%M:%S') < datetime.strptime('2021-12-21', '%Y-%m-%d'):
+                break
+            id = jobpost['id']
+            yield scrapy.Request(url=f'https://job.bupt.edu.cn/f/recruitmentinfo/ajax_show?recruitmentId={id}', 
+                                 callback=self.parse_details, meta={'method': "stastic"})
+        else:
+            self.n=data['object']['pageNo']+1
+            if not data['object']['lastPage']:
+                yield scrapy.Request(url=f'https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo={self.n}', 
+                                     callback=self.parse, meta={'method': "stastic"})
+        
     def parse_details(self, response):
-        #pass
         item = Jobtop10Item()
-        print(response.url)
+        data = json.loads(response.body)
+        job = data['object']['recruitmentinfo']
+        item['job_title'] = job['title']
+        item['job_date'] = job['startTime']
+        item['job_views'] = job['browseNumber']
+        item['job_nums'] = job['positionNum']
+        
+        yield item

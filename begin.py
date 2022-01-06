f1f2b617aa47b9e1e6ac7aafac2d504aa75aea7f
LiNY2 <1623482168@qq.com>
2022-01-06 11:12:11 +0800

    modified and found a jb-bug

--- begin.py
+++ begin.py
@@ -1,3 +1,4 @@
 from scrapy import cmdline
-cmdline.execute("scrapy multi_crawl BUPTJOB".split())
+cmdline.execute("scrapy multi_crawl".split())
+# cmdline.execute("scrapy multi_crawl BUPTJOB".split())
 # cmdline.execute("scrapy multi_crawl BEIDA_JIUYE_ALL".split())

73714063e04acc6581ca249277f50ed51caa0554
LiNY2 <1623482168@qq.com>
2022-01-07 21:57:03 +0800

    add commit

--- JOBTOP10/spiders/BUPTJOB_spider.py
+++ JOBTOP10/spiders/BUPTJOB_spider.py
@@ -12,31 +12,31 @@ class BUPTJOBSpider(scrapy.spiders.Spider):
     name="BUPTJOB"
     allowed_domains = ["job.bupt.edu.cn"]
     # start_urls = ['https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo=']
-    
     pagenum=1
     def start_requests(self):
+        #由于start_urls不能传静态爬取参数，用start_requests
         yield scrapy.Request('https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo=',
                              callback=self.parse, meta={'method': "stastic"})
-   
     def parse(self, response):
-        data = json.loads(response.body)   # 获取网页的json数据
+        data = json.loads(response.body)   
+        # 获取网页的json数据
         jobpost_list = data['object']['list']
         print("[BUPT]now pagenum:"+str(data['object']['pageNo']))
         print("[BUPT]job len:"+str(len(jobpost_list)))
         for jobpost in jobpost_list:
-
             if datetime.strptime(jobpost["startTime"], '%Y-%m-%d %H:%M:%S') < datetime.strptime('2021-09-01', '%Y-%m-%d'):
                 break
             id = jobpost['id']
+            #获取下一页url，到二级页面爬取
             yield scrapy.Request(url=f'https://job.bupt.edu.cn/f/recruitmentinfo/ajax_show?recruitmentId={id}', 
-                                 callback=self.parse_details, meta={'method': "stastic"})
-        
+                                 callback=self.parse_details, meta={'method': "stastic"})#回调函数改变
         self.n=data['object']['pageNo']+1
         if not data['object']['lastPage']:
+            #判断尾页
             yield scrapy.Request(url=f'https://job.bupt.edu.cn/f/recruitmentinfo/ajax_frontRecruitinfo?pageNo={self.n}', 
                                     callback=self.parse, meta={'method': "stastic"})
-        
     def parse_details(self, response):
+        #爬详情页，代码非常简单
         item = Jobtop10Item()
         data = json.loads(response.body)
         job = data['object']['recruitmentinfo']
@@ -44,5 +44,4 @@ class BUPTJOBSpider(scrapy.spiders.Spider):
         item['job_date'] = job['startTime']
         item['job_views'] = job['browseNumber']
         item['job_nums'] = job['positionNum']
-        
         yield item
--- JOBTOP10/spiders/XDUJOB_spider.py
+++ JOBTOP10/spiders/XDUJOB_spider.py
@@ -10,61 +10,57 @@ from selenium.webdriver import FirefoxOptions
 
 ##爬到十八页
 class XDUJOBSpider(scrapy.spiders.Spider):
-    name = "XDUJOB"  # 定义爬虫的名字
-    allowed_domains = ["job.xidian.edu.cn"]#描述网站域名
+    name = "XDUJOB"  # 爬虫的名字
+    allowed_domains = ["job.xidian.edu.cn"]#允许访问的网站域名
     start_urls = ["https://job.xidian.edu.cn/campus/index?domain=xidian&city=&page=1"]
-
+    #起始页
     xidian_next_page=''
-
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         spider = super(XDUJOBSpider, cls).from_crawler(
             crawler, *args, **kwargs)
-        # 创建 浏览器
+        # 创建浏览器
         spider.driver = webdriver.Firefox()
         crawler.signals.connect(spider.spider_closed,
                                 signal=signals.spider_closed)
         return spider
-
     def spider_closed(self, spider):
         spider.driver.close()  # 关闭浏览器
         print("==========爬虫结束！==========")
         spider.logger.info('Spider closed:%s', spider.name)
 
     def parse(self, response): #解析爬取的内容
-        item = Jobtop10Item()  # 生成在items.py中定义的Item对象,用于接收爬取的数据
-        #下一页的url，存在于列表页的class="next"的li里面的a标签的href中；注意是相对路径
+        item = Jobtop10Item()
+        #下一页的url
         next_page_href = response.css('li[class="next"]>a::attr(href)').extract()
-        # 末尾页的url，存在于列表页的class="last"的li里面的a标签的href中；注意是相对路径
+        # 末尾页的url
         last_page_href = response.css('li[class="last"]>a::attr(href)').extract()
+        #确认现在页面不是最后一页
+        #在尾页页的时候页面发生改变，尾页的链接在li[class="last "]下
+        #按上面代码无法获取，由于只有一页，直接用len判断
         if response.url != last_page_href and len( last_page_href) :
             self.xidian_next_page = 'https://job.xidian.edu.cn' + next_page_href[0]
         else:
-            self.xidian_next_page = ''
-        
+            self.xidian_next_page = ''   
         c_page_url_list = response.css('ul[class="infoList"]>li:nth-child(1)>a')
         for job in c_page_url_list:  # 循环打开和解析每个详情页
-            #此处，调用下载中间件类中的XIDIAN_driver，进行详情页的打开
-            driver=self.driver
+            driver=self.driver #用爬虫定义的driver
             driver.get('https://job.xidian.edu.cn' + job.css('a::attr(href)').extract()[0])
-            time.sleep(2)#等待几秒钟
-            # class ="name text-primary"的a标签的文本，就是招聘主题；
-            #注意下面的语法是selenium的find_element函数的语法；通过.text,获取对象的text；另注意要返回列表给item对象
+            time.sleep(2)#等待2秒钟
             item['job_title'] = [driver.find_element(
                 'css selector', 'div[class="title-message"]>h5').text][0]
-            # class ="share"的div下面的ul下面的第1个li的文本，就是发布时间
             date_text = driver.find_element('css selector', 'div[class="share"]>ul>li:nth-child(1)').text
-            date_text=date_text[date_text.find('：') + 1:] #取“：”后面的日期；西电样例：“发布时间：2021-12-03 13:21”
+            date_text=date_text[date_text.find('：') + 1:] 
             if datetime.strptime(date_text,'%Y-%m-%d %H:%M')<datetime.strptime('2021-09-01 00:00','%Y-%m-%d %H:%M'):
-                #当读取出来的日期已经比设定的还早，说明已经不需要读取了，直接退出循环
-                self.xidian_next_page=''#退出前给该处置空，不用再继续执行其他列表页了
-                break#测试时候设定时间为'2021-12-03'，正式执行时候，此处时间按要求设置为'2021-09-01'dri
-            item['job_date'] = [date_text][0]  #赋值为列表，传递给item:
-            # class ="share"的div的ul的第2个li中，就是浏览次数：“浏览次数：6”
+                self.xidian_next_page=''
+                #到了设定时间，停止爬取。刚好可以利用下一页的url作为结束标志
+                break
+            item['job_date'] = [date_text][0] 
             views_text = driver.find_element('css selector', 'div[class="share"]>ul>li:nth-child(2)').text
-            item['job_views'] = [views_text[views_text.find('：') + 1:]][0]  # 先获取文本，再截取“：”后面的次数
+            item['job_views'] = [views_text[views_text.find('：') + 1:]][0]  
             item['job_nums']=1
-            yield item#提交给pipelines
+            yield item
         #处理完列表页的所有二级页面后，继续打开下一页进行抓取
         if self.xidian_next_page!='':
+        #如果没有下一页了，停止爬取
             yield scrapy.Request(self.xidian_next_page, callback=self.parse)
